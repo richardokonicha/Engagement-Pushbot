23ced554f0beb93bf5df55a0e1b198c4c8a4d880
reechee <richardokonicha@gmail.com>
2020-04-21 06:10:46 +0100

    create rounds and memberlist class and functions

--- database/db.py
+++ database/db.py
@@ -1,8 +1,10 @@
+
 import sqlalchemy
-from sqlalchemy import Column, VARCHAR, Integer, String, DateTime
+from sqlalchemy import Column, VARCHAR, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.sql import exists
 import datetime
 
 SQLITE = 'sqlite:///database/database.db'
@@ -13,6 +15,7 @@ Base = declarative_base()
 
 
 class Users(Base):
+    """User class"""
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
@@ -24,10 +27,12 @@ class Users(Base):
     pool_count = Column(Integer)
 
     def commit(self):
+        """commits query object to db"""
         session.add(self)
         session.commit()
 
     def warning(self):
+        """increament warn by 1 everytime its called"""
         try:
             self.warns +=1
         except TypeError:
@@ -36,9 +41,9 @@ class Users(Base):
             return self.warns
         session.commit()
         
-
     @classmethod
     def get(cls, userid):
+        """retrive user from id"""
         user = session.query(cls).filter_by(user_id=userid).first()
         if user:
             return user
@@ -46,19 +51,124 @@ class Users(Base):
             return None
     @classmethod
     def create(cls, userid, name):
+        """create new users by passing user id and name"""
         user = cls(
             user_id=userid,
             name=name
             )
+        session.add(user)
         session.commit()
         return user
 
     def delete(self):
+        """delete user object"""
         session.delete(self)
         session.commit()
 
     def __repr__(self):
         return f"User {self.name} {self.user_id}"
 
-# Base.metadata.create_all(engine)
 
+class Rounds(Base):
+    __tablename__="rounds"
+    id = Column(Integer, primary_key=True)
+    start_time = Column(DateTime)
+    memberlist = relationship("MemberList", uselist=True, backref="round")
+
+    def __init__(self, start_time, str=False):
+        """initializes rounds and set start time"""
+        if type(start_time) is not str:
+            self.start_time = datetime.datetime.fromisoformat(start_time)
+        else:
+            self.start_time = start_time
+
+    @classmethod
+    def create(cls, start_time):
+        """create round function by passing in time"""
+        rounds = cls(
+            start_time
+            )
+        session.add(rounds)
+        session.commit()
+        return rounds
+
+    def start(self):
+        """retrieve the start time of round"""
+        return self.start_time
+
+    def end(self):
+        """retrieve the end time of round"""
+        return self.start_time + datetime.timedelta(minutes=3)
+
+    def join(self, user):
+        """adds user to round by passing in the user object"""
+        user_id = user.user_id
+        if MemberList.exist(user_id):
+            return True
+        else:
+            entry = MemberList(
+                round_id=self.id,
+                user=user
+                )
+            session.add(entry)
+            session.commit()
+    
+    @classmethod
+    def get_round(cls, id):
+        """get round object by id"""
+        return session.query(cls).filter_by(id=id).first()
+
+    @classmethod
+    def get_memberList(cls, id):
+        """get list of all members in that round"""
+        return session.query(MemberList).all()
+
+    @classmethod
+    def get_lastRound(cls):
+        """get last round"""
+        #TODO get last round
+        allr = session.query(cls).all()
+
+    def commit(self):
+        """add and commit session changes to db"""
+        session.add(self)
+        session.commit()
+
+    def __repr__(self):
+        """string representation of object"""
+        return f"Round {self.id} {str(self.start_time)}"
+
+
+class MemberList(Base):
+    """member list class"""
+    __tablename__="memberlist"
+    id = Column(Integer, primary_key=True)
+    round_id = Column(Integer, ForeignKey("rounds.id"))
+    #userinfo
+    user_id = Column(Integer)
+    name = Column(String)
+    username = Column(String)
+
+    def __init__(self, round_id, user):
+        self.round_id = round_id
+        self.user_id = user.user_id
+        self.name = user.name
+        self.username = user.username
+
+    @classmethod
+    def all(cls):
+        return session.query(cls).all()
+
+    @classmethod
+    def exist(cls,user_id):
+        """checks if user is on the list returns boolean"""
+        return session.query(exists().where(cls.user_id==user_id)).scalar()
+
+    def __repr__(self):
+        return f"MemberList {self.name} round{self.round_id}"
+
+
+Base.metadata.create_all(engine)
+
+
+# import pdb; pdb.set_trace()
--- epush_bot.py
+++ epush_bot.py
@@ -6,6 +6,8 @@ import datetime
 
 bot = telebot.TeleBot(TOKEN, threaded=True)
 
+############################################################ MARKUPS
+
 markup =  telebot.types.InlineKeyboardMarkup()
 engagement_button = telebot.types.InlineKeyboardButton(text="Engagement", callback_data="engagement")
 warn_button = telebot.types.InlineKeyboardButton(text="Warn", callback_data="warn")
@@ -14,6 +16,7 @@ markup.add(engagement_button, warn_button)
 register_markup = telebot.types.InlineKeyboardMarkup()
 register_button = telebot.types.InlineKeyboardButton(text="Register with Us", callback_data="register_member")
 register_markup.add(register_button)
+
 
 input_markup = telebot.types.InlineKeyboardMarkup()
 input_button = telebot.types.InlineKeyboardButton("Username", callback_data="input_user")
@@ -25,9 +28,57 @@ dashboard_markup = telebot.types.InlineKeyboardMarkup()
 dashboard_button = telebot.types.InlineKeyboardButton(text="Go to Dashboard", callback_data="dashboard")
 dashboard_markup.add(dashboard_button)
 
+dashview_markup = telebot.types.InlineKeyboardMarkup()
+u_btn = telebot.types.InlineKeyboardButton("Edit Username", callback_data="input_user")
+e_btn = telebot.types.InlineKeyboardButton("Engagement", callback_data="engagement")
+w_btn = telebot.types.InlineKeyboardButton("Warns", callback_data="warns")
+# d_btn = telebot.types.InlineKeyboardButton("<<< Go to Dashboard       Next round in 34sec", callback_data="dashboard")
+dashview_markup.add(u_btn)
+dashview_markup.add(e_btn, w_btn)
+# dashview_markup.add(d_btn)
+
+#############################################################     FUNCT     #######################
 
 
-#################################################################### START HERE #######################
+def input_user_account(message):
+    chat_id = message.chat.id
+    IG_username = message.text
+    name = message.from_user.first_name
+    user_id = message.from_user.id
+    epush_user = db.Users.get(user_id)
+    if epush_user == None:
+        epush_user = db.Users(
+            user_id=user_id,
+            name=name,
+            username=IG_username,
+            join_date=datetime.datetime.now()
+        )
+        epush_user.commit()
+        text = f"""
+Congratulations !! You're now a member of epush engagement pool
+Your Instagram username is set to <b>@{IG_username}</b>
+    """
+        bot.send_message(
+                chat_id,
+                text=text,
+                parse_mode="html",
+                reply_markup=dashboard_markup
+                )
+    else:
+        text = f"""
+Your Instagram Username has been changed to {IG_username}
+        """
+        bot.send_message(
+            chat_id,
+            text=text,
+            parse_mode="html",
+            reply_markup=dashboard_markup
+            )
+
+
+#################################################################### HANDLERS START HERE #######################
+
+
 @bot.message_handler(commands=["start"])
 def start(message):
     chat_id = message.chat.id
@@ -46,31 +97,63 @@ and set your instagram account username e.g @reechee
     bot.send_message(chat_id, text=start_text, reply_markup=register_markup, parse_mode="html")
 
 
-
-def input_user_account(message):
-    chat_id = message.chat.id
-    IG_username = message.text
-    name = message.from_user.first_name
+@bot.message_handler(commands=["dashboard"])
+def dashboard(message):
     user_id = message.from_user.id
-    epush_user = db.Users(
-        user_id=user_id,
-        name=name,
-        username=IG_username,
-        join_date=datetime.datetime.now()
-    )
-    epush_user.commit()
-    text = f"""
-Congratulations !! You're now a member of epush engagement pool
-Your Instagram username is set to <b>@{IG_username}</b>
+    message_id = message.json['message_id']
+    epush_user = db.Users.get(user_id)
+    dashboard_text = """
+This is the Dashboard view
+Next engage is 2hours
+You would get a reminder 30mins before round starts
+
     """
     bot.send_message(
-        chat_id,
-        text=text,
-        parse_mode="html",
-        reply_markup=dashboard_markup
+        user_id,
+        text=dashboard_text,
+        reply_markup=dashview_markup
         )
+    # bot.edit_message_text(
+    #     dashboard_text,
+    #     user_id,
+    #     message_id=message_id,
+    #     reply_markup=dashview_markup
+    # )
 
 
+@bot.message_handler(commands=["round"])
+def round(message):
+    user_id = message.from_user.id
+    epush_user = db.Users.get(user_id)
+
+
+    text="""
+Engagement push round starts in 30minutes,
+If you wish to join the next round 
+Click you username to to join round
+    """
+    btn_text=f"Join round as: @{epush_user.username}"
+
+    usern_mrkp = telebot.types.InlineKeyboardMarkup()
+    usern_btn = telebot.types.InlineKeyboardButton(text=btn_text, callback_data="join_round")
+    usern_mrkp.add(usern_btn)
+    bot.send_message(
+        user_id,
+        text=text,
+        reply_markup=usern_mrkp,
+        parse_mode="html"
+    )
+
+
+
+
+
+
+
+############################################################## CALL BACKS  ######################################33
+
+
+######## REGISTER
 @bot.callback_query_handler(func=lambda call: call.data=="register_member")
 def callback_hand(call):
     chat_id = call.message.chat.id
@@ -79,9 +162,9 @@ def callback_hand(call):
     name = call.from_user.first_name
     message_id = call.message.json['message_id']
     epush_user = db.Users.get(user_id)
-    epush_user.delete()
-    epush_user=None
-    if epush_user:
+    # epush_user.delete()
+    # epush_user=None
+    if epush_user != None:
         username = epush_user.username
         text = f"""
 Ooops <b>{name}</b> you are already registered with us.
@@ -99,26 +182,72 @@ You can also contact support to resolve any problem /support.
             )
         bot.register_for_reply_by_message_id(message_id+1, input_user_account)
 
-#         text = """
-# Congratulations !!
-# You're now a member of epush engagement pool
-# Details of the rounds yadayada
-# Goto your dashboard to view details and information on the rounds and stats
-#         """
-#         epush_user = db.Users.create(user_id, name)
-#         epush_user.commit()
-#         bot.send_message(chat_id, text=text, reply_markup=register_markup)
+####### _change user
+@bot.callback_query_handler(func=lambda call: call.data=="input_user")
+def input_user(call):
+    user_id = call.from_user.id
+    message_id = call.message.json['message_id']
+    epush_user = db.Users.get(user_id)
+    bot.send_message(
+        user_id, 
+        text="Enter your instagram username/handle e.g @reechee",
+        reply_markup=force_reply
+        )
+    bot.register_for_reply_by_message_id(message_id+1, input_user_account)
+
+######## warns
+@bot.callback_query_handler(func=lambda call: call.data=="warns")
+def warns(call):
+    user_id = call.from_user.id
+    message_id = call.message.json['message_id']
+    epush_user = db.Users.get(user_id)
+    text = """
+A warn is a count of the number of times
+you have defaulted.
+You can default by joining a round and failing to like the last post of
+every member in that round. 
+Warn count: <b>2</b>
+    """
+    bot.edit_message_text(
+        text=text,
+        chat_id=user_id,
+        message_id=message_id,
+        parse_mode="html"
+        # reply_markup=dashview_markup
+    )
+
+
+######## _dashboard_
+@bot.callback_query_handler(func=lambda call: call.data=="dashboard")
+def dashboard(call):
+    user_id = call.from_user.id
+    message_id = call.message.json['message_id']
+    epush_user = db.Users.get(user_id)
+    dashboard_text = """
+This is the Dashboard view
+Next engage is 2hours
+You would get a reminder 30mins before round starts
+
+    """
+    bot.send_message(
+        user_id,
+        text=dashboard_text,
+        reply_markup=dashview_markup
+        )
+    # bot.edit_message_text(
+    #     dashboard_text,
+    #     user_id,
+    #     message_id=message_id,
+    #     reply_markup=dashview_markup
+    # )
 
 
 
-
-  
-
-    # bot.answer_callback_query(callback_query_id=callback_query_id, text="Thanks for registering")
-
-
+#### ROUND
+@bot.callback_query_handler(func=lambda call: call.data=="join_round")
+def join_round(message):
+    
 
 
-
-# bot.remove_webhook()
+bot.remove_webhook()
 bot.polling()
