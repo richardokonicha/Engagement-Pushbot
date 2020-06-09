1db2bdf68063bc86d18ccc8d14b54d5ce0f4abeb
reechee <richardokonicha@gmail.com>
2020-06-09 15:07:24 +0100

    redis config

--- worker.py
+++ worker.py
@@ -7,7 +7,7 @@ listen = ['high', 'default', 'low']
 
 redisi = "redis://h:pf148522bd570fe8cc658f2b035320b318d584d740875f42f613ce6725077fbd0@ec2-18-200-224-51.eu-west-1.compute.amazonaws.com:16339"
 
-redis_url = os.getenv('REDIS_URL', redisi)
+redis_url = os.getenv('REDISTOGO_URL', redisi)
 
 # redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
