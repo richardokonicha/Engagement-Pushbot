9ccf663bd944825b8092056a2132d5f2ad318c3a
reechee <richardokonicha@gmail.com>
2020-06-09 15:03:27 +0100

    mend

--- worker.py
+++ worker.py
@@ -1,26 +1,19 @@
-
-# background process
 import os
-import redis
-from rq import Queue, Worker, Connection
 
-listen = ["high", "default", "low"]
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
 
 redisi = "redis://h:pf148522bd570fe8cc658f2b035320b318d584d740875f42f613ce6725077fbd0@ec2-18-200-224-51.eu-west-1.compute.amazonaws.com:16339"
 
 redis_url = os.getenv('REDIS_URL', redisi)
 
-# conn = redis.from_url(redis_url)
+# redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
-conn = redis.Redis(
-    host='redis-13324.c89.us-east-1-3.ec2.cloud.redislabs.com', 
-    port=13324, 
-    password="konichar"
-    )
+conn = redis.from_url(redis_url)
 
-print("redis url set", conn)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
